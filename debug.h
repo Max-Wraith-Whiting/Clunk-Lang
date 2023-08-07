02d1a0477b6dee5e7dcc41a24ebcfae4c1290cfd
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-07 15:37:35 +0100

    Added debug header for disassmebler

---
+++ debug.h
@@ -0,0 +1,9 @@
+#ifndef clunk_debug_h
+#define clunk_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
