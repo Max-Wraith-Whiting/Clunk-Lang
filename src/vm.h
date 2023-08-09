a6a6c90b7024b6bf2d0075ae614b1dd32f2049a5
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-09 16:51:16 +0100

    declared VM stack - Added helper functions for stack interaction

--- src/vm.h
+++ src/vm.h
@@ -2,10 +2,15 @@
 #define clunk_vm_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct {
     Chunk* chunk;
     uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
 } VM;
 
 typedef enum {
@@ -18,5 +23,7 @@ void initVM();
 void freeVM();
 
 InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+void pop();
 
 #endif
