9fc7db3e59033ce89a69a6abe9239e0569fa00ea
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-11 17:12:29 +0100

    Added scanner literals support (strings & numbers)

--- src/scanner.c
+++ src/scanner.c
@@ -18,6 +18,10 @@ void initScanner(const char* source) {
     scanner.line = 1;
 }
 
+static bool isDigit(char c) [
+    return c >= '0' && c <= '9'; 
+]
+
 static bool isAtEnd() {
     return *scanner.current == '\0';
 }
@@ -88,6 +92,31 @@ static void skipWhitespace() {
     }
 }
 
+static Token number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a decimal part.
+    if (peek() == '.' && isDigit(peekNext())) {
+        advance(); // Consume the ".".
+
+        while(isDigit(peek())) advance();
+    }
+
+    return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') scanner.line++;
+        advance();
+    }
+
+    if (isAtEnd()) return errorToken("Unterminated string.");
+
+    advance(); // Consumes second quote.
+    return makeToken(TOKEN_STRING); 
+}
+
 Token scanToken() {
     skipWhitespace();
     scanner.start = scanner.current;
@@ -95,6 +124,7 @@ Token scanToken() {
     if (isAtEnd()) return makeToken(TOKEN_EOF);
 
     char c = advance();
+    if (isDigit(c)) return number();
 
     switch (c) {
         case '(': return makeToken(TOKEN_LEFT_PAREN);
@@ -120,6 +150,8 @@ Token scanToken() {
         case '>':
             return makeToken(
                 match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+        case '"':
+            return string();
     }
 
     return errorToken("Unexpected character.");
