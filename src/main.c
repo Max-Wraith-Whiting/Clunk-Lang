ce79bff5436c4f29e4c97fa640dd1cd164551920
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-11 15:50:48 +0100

    + basic REPL method

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
+
+static void repl() {
+    char line[1024];
+    for (;;) {
+        printf("> ");
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
 
 int main(int argc, const char* argv[]) {
     initVM();
