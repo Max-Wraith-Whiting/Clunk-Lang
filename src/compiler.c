1caf8e71f0f38d7f2f7dede10cd5d9884b39535e
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-14 15:57:33 +0100

    Removed dummy compiler, replaced with new beginning

--- src/compiler.c
+++ src/compiler.c
@@ -6,20 +6,7 @@
 
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
-
-    // DUMMY COMPILER!!!
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-        if (token.type == TOKEN_EOF) break;
-    }
-    // DUMMY COMPILER!!!
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expressin.");
 }
