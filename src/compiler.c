4721b41093b5e7dc755c1879f6bb2b2c852f1bec
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-15 17:08:28 +0100

    Precedence enum added

--- src/compiler.c
+++ src/compiler.c
@@ -12,6 +12,20 @@ typedef struct {
     bool panicMode;
 } Parser;
 
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,  // =
+    PREC_OR,          // or
+    PREC_AND,         // and
+    PREC_EQUALITY,    // == !=
+    PREC_COMPARISON,  // < > <= >=
+    PREC_TERM,        // + -
+    PREC_FACTOR,      // * /
+    PREC_UNARY,       // ! -
+    PREC_CALL,        // . ()
+    PREC_PRIMARY
+} Precedence;
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -118,7 +132,7 @@ static void unary() {
 }
 
 static void parsePrecedence(Precedence precedence) {
-    
+
 }
 
 static void expression() {
