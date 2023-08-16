ca2d7dfefe373ab865f1e6b830257c30c2cee8be
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-16 15:56:24 +0100

    [BUGFIX] '()' added to line 144 for func call

--- src/scanner.c
+++ src/scanner.c
@@ -141,7 +141,7 @@ static TokenType identifierType() {
 }
 
 static Token identifier() {
-    while (isAlpha(peek()) || isDigit(peek())) advance;
+    while (isAlpha(peek()) || isDigit(peek())) advance();
     return makeToken(identifierType());
 }
 
