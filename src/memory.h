fddc4c24246fff4b719c107a71ebfee32659f477
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-28 14:53:02 +0100

    Declared FREE macro to handle freeing memory in GC readable way

--- src/memory.h
+++ src/memory.h
@@ -2,12 +2,15 @@
 #define clunk_memory_h
 
 #include "common.h"
-
-#define GROW_CAPACITY(capacity) \
-    ((capacity) < 8 ? 8 : (capacity) * 2)
+#include "object.h"
 
 #define ALLOCATE(type, count) \
     (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0);
+
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount) \
     (type*)reallocate(pointer, sizeof(type) * (oldCount), \
@@ -18,5 +21,6 @@
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
+void freeObjects();
 
 #endif
