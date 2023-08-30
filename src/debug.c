1f62dcbd6ec7a1f400f9894732828116f8eeb0e6
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-08-30 15:56:52 +0100

    Added debug support for print statement

--- src/debug.c
+++ src/debug.c
@@ -44,6 +44,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_LESS:       return simpleInstruction("OP_LESS", offset);
         case OP_NEGATE:     return simpleInstruction("OP_NEGATE", offset);
         case OP_NOT:        return simpleInstruction("OP_NOT", offset);
+        case OP_PRINT:      return simpleInstruction("OP_PRINT", offset);
         case OP_RETURN:     return simpleInstruction("OP_RETURN", offset);
         case OP_ADD:        return simpleInstruction("OP_ADD", offset);
         case OP_SUBTRACT:   return simpleInstruction("OP_SUBTRACT", offset);
