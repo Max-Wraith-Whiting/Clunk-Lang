155204b00056492a750f750fb8c4e3771b005a95
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-09-03 19:11:42 +0100

    Dissassembly support for setters

--- src/debug.c
+++ src/debug.c
@@ -41,6 +41,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_FALSE:         return simpleInstruction("OP_FALSE", offset);
         case OP_POP:           return simpleInstruction("OP_POP", offset);
         case OP_GET_GLOBAL:    return constantInstruction("OP_GET_GLOBAL", chunk, offset);
+        case OP_SET_GLOBAL:    return constantInstruction("OP_SETE_GLOBAL", chunk, offset);
         case OP_EQUAL:         return simpleInstruction("OP_EQUAL", offset);
         case OP_DEFINE_GLOBAL: return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
         case OP_GREATER:       return simpleInstruction("OP_GREATER", offset);
