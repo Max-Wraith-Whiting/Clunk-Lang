83feab3d99d106e75098bb7e33fa69f3dcad8481
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-09-07 14:56:46 +0100

    For loop now considers init clause

--- src/compiler.c
+++ src/compiler.c
@@ -480,8 +480,15 @@ static void expressionStatement() {
 }
 
 static void forStatement() {
+    beginScope();
     consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
-    consume(TOKEN_SEMICOLON, "Expect ';'.");
+    if (match(TOKEN_SEMICOLON)) {
+        // No initialiser provided.
+    } else if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        expressionStatement();
+    }
 
     int loopStart = currentChunk()->count;
     consume(TOKEN_SEMICOLON, "Expect ';'.");
@@ -489,6 +496,7 @@ static void forStatement() {
 
     statement();
     emitLoop(loopStart);
+    endScope();
 }
 
 static void ifStatement() {
