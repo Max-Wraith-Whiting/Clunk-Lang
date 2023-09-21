4b48b9f5e1ee76471d8e1920cca3b201053d97b3
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-09-21 17:51:09 +0100

    Added a way to define a C function as a NativeFn

--- src/vm.c
+++ src/vm.c
@@ -38,6 +38,14 @@ static void runtimeError(const char* format, ...) {
     resetStack();
 }
 
+static void defineNative(const char* name, NativeFn function) {
+    push(OBJ_VAL(copyString(name, (int)strlen(name))));
+    push(OBJ_VAL(newNative(function)));
+    tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+    pop();
+    pop();
+}
+
 void initVM() {
     resetStack();
     vm.objects = NULL;
