2d44be5c41941fd94706ec154afb86ea3d5ea9e6
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-09-21 17:13:15 +0100

    Initialising top-level call frame via call()

--- src/vm.c
+++ src/vm.c
@@ -253,10 +253,6 @@ InterpretResult interpret(const char* source) {
     if (function == NULL) return INTERPRET_COMPILE_ERROR;
 
     push(OBJ_VAL(function));
-    CallFrame* frame = &vm.frames[vm.frameCount++];
-    frame->function = function;
-    frame->ip = function->chunk.code;
-    frame->slots = vm.stack;
-
+    call(function, 0);
     return run();
 }
