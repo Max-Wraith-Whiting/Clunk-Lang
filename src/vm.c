e354ba8a4ced6e41de624e42d2d46e7c7c66d6c7
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-09-21 17:53:08 +0100

    Declaring native FN "clock"

--- src/vm.c
+++ src/vm.c
@@ -11,6 +11,10 @@
 
 VM vm;
 
+static Value clockNative(int argCount, Value* args) {
+    return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
+
 static void resetStack() {
     vm.stackTop = vm.stack;
     vm.frameCount = 0;
